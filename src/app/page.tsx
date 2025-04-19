ab53e405ea0468d3993ecf525a3c75437fb7578b
Nadil Kumbukage <z5525475@ad.unsw.edu.au>
2025-04-20 00:10:45 +1000

    include player component in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./globals.css";
 import Hero from "@/components/Hero";
+import Player from "@/components/Player";
 
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <Player/>
       <div className="h-[4000px]"></div>
     </main>
   );
