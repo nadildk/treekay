e6aef99d5efdb5a2140e933f2f386cfc60663ef6
Nadil K <102883437+nadildk@users.noreply.github.com>
2025-04-20 12:57:20 +1000

    add formatting for the player controls and add cover image to player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -3,17 +3,31 @@
 import React from "react";
 import Image from "next/image";
 import { AudioPlayer } from "react-audio-play";
-import { motion } from "framer-motion";
-import { fadeIn } from "../../variants";
+// import { motion } from "framer-motion";
+// import { fadeIn } from "../../variants";
 
 const Player = () => {
   return (
-    <div className="w-full">
-      <div className="container mx-auto px-4">
-        {/* text & avatar img */}
-        <div>text & avatar img</div>
-        {/* player */}
-        <div className="w-full">
+    <div
+      className="w-full bg-gradient-to-r from-zinc-800 to-black backdrop-blur-
+    [15px] flex items-center relative z-40 py-4 xl:py-0"
+    >
+      <div className="container mx-auto flex flex-col xl:flex-row items-center xl:items-center gap-4 w-full">
+        {/* left: cover image */}
+        <div className="hidden xl:flex items-center gap-x-4 w-[300px] shrink-0">
+          <div className="w-[96px] h-[96px] relative">
+            <Image
+              src={"/treekay_cover.png"}
+              alt="cover"
+              fill
+              className="object-cover rounded-md"
+              priority
+            />
+          </div>
+        </div>
+
+        {/* audio player */}
+        <div className="flex-1 w-full px-4 xl:px-0">
           <AudioPlayer
             loop
             preload="none"
@@ -22,12 +36,11 @@ const Player = () => {
             volumePlacement="top"
             src="/build_a_soul_beat.wav"
             style={{
-                background: 'transparent',
-                boxShadow: 'none',
-                width: '100%',
-                maxWidth: "100%",
+              background: "transparent",
+              boxShadow: "none",
+              width: "100%",
+              maxWidth: "100%",
             }}
-
           />
         </div>
       </div>
