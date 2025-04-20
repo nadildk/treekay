5d587b2bae9bc1c4cfab191cc4a7efb83b9e1480
Nadil K <102883437+nadildk@users.noreply.github.com>
2025-04-20 16:53:42 +1000

    complete initial functional player with image, text and playback features

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -3,19 +3,22 @@
 import React from "react";
 import Image from "next/image";
 import { AudioPlayer } from "react-audio-play";
-// import { motion } from "framer-motion";
-// import { fadeIn } from "../../variants";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const Player = () => {
   return (
-    <div
-      className="w-full bg-gradient-to-r from-zinc-800 to-black backdrop-blur-
+    <motion.div
+      variants={fadeIn("up", 0.5)}
+      initial="hidden"
+      whileInView={"show"}
+      className="w-full h-[112px] bg-gradient-to-r from-zinc-800 to-black backdrop-blur-
     [15px] flex items-center relative z-40 py-4 xl:py-0"
     >
       <div className="container mx-auto flex flex-col xl:flex-row items-center xl:items-center gap-4 w-full">
-        {/* left: cover image */}
         <div className="hidden xl:flex items-center gap-x-4 w-[300px] shrink-0">
-          <div className="w-[96px] h-[96px] relative">
+          {/* cover image */}
+          <div className="w-[86px] h-[86px] relative">
             <Image
               src={"/treekay_cover.png"}
               alt="cover"
@@ -23,6 +26,11 @@ const Player = () => {
               className="object-cover rounded-md"
               priority
             />
+          </div>
+          {/* text */}
+          <div className="leading-none text-white">
+            <div className="text-lg font-medium">build a soul beat</div>
+            <div className="text-sm font-light">beat tape #2</div>
           </div>
         </div>
 
@@ -44,7 +52,7 @@ const Player = () => {
           />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
